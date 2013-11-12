25e312b4fafb418cfab97e27ea7a04a6424e5a20
Toru Nayuki <tnayuki@MacBook-Air.local>
2013-11-12 23:12:06 +0900

    libFTDIを使って再実装。

--- ArtNetNodeMac/AppDelegate.m
+++ ArtNetNodeMac/AppDelegate.m
@@ -12,39 +12,99 @@
 
 #include <artnet/artnet.h>
 #include <artnet/packets.h>
-
-extern void *opendmx_open(void);
-extern void opendmx_send_frame(void *fthandle, unsigned numchannels, uint8_t *buf);
+#include <ftdi.h>
+#include <libusb.h>
 
 static dispatch_semaphore_t semaphore;
 static unsigned char data[512];
-static void *h;
-static struct timeval ot;
+
+struct ftdi_context ftdic;
+
+#define DMX_MAB 160    // Mark After Break 8 uS or more
+#define DMX_BREAK 110  // Break 88 uS or more
+
+static int do_dmx_break(struct ftdi_context* ftdic)
+{
+    int ret;
+    
+	if ((ret = ftdi_set_line_property2(ftdic, BITS_8, STOP_BIT_2, NONE, BREAK_ON)) < 0)
+	{
+        fprintf(stderr, "unable to set BREAK ON: %d (%s)\n", ret, ftdi_get_error_string(ftdic));
+        return EXIT_FAILURE;
+	}
+    usleep(DMX_BREAK);
+	if ((ret = ftdi_set_line_property2(ftdic, BITS_8, STOP_BIT_2, NONE, BREAK_OFF)) < 0)
+	{
+        fprintf(stderr, "unable to set BREAK OFF: %d (%s)\n", ret, ftdi_get_error_string(ftdic));
+        return EXIT_FAILURE;
+	}
+    usleep(DMX_MAB);
+	return EXIT_SUCCESS;
+}
+
+static int dmx_init(struct ftdi_context* ftdic)
+{
+    int ret;
+    
+    if (ftdi_init(ftdic) < 0)
+    {
+        fprintf(stderr, "ftdi_init failed\n");
+        return EXIT_FAILURE;
+    }
+    
+    if ((ret = ftdi_usb_open(ftdic, 0x0403, 0x6001)) < 0)
+    {
+        fprintf(stderr, "unable to open ftdi device: %d (%s)\n", ret, ftdi_get_error_string(ftdic));
+        return EXIT_FAILURE;
+    }
+    
+    if ((ret = ftdi_set_baudrate(ftdic, 250000)) < 0)
+	{
+        fprintf(stderr, "unable to set baudrate: %d (%s)\n", ret, ftdi_get_error_string(ftdic));
+        return EXIT_FAILURE;
+	}
+    
+	if ((ret = ftdi_set_line_property(ftdic, BITS_8, STOP_BIT_2, NONE)) < 0)
+	{
+        fprintf(stderr, "unable to set line property: %d (%s)\n", ret, ftdi_get_error_string(ftdic));
+        return EXIT_FAILURE;
+	}
+    
+    if ((ret = ftdi_setflowctrl(ftdic, SIO_DISABLE_FLOW_CTRL)) < 0)
+	{
+        fprintf(stderr, "unable to set flow control: %d (%s)\n", ret, ftdi_get_error_string(ftdic));
+        return EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
+}
+
+static int dmx_write(struct ftdi_context* ftdic, unsigned char* dmx, size_t size)
+{
+    int ret;
+    
+	if ((ret = do_dmx_break(ftdic)) == EXIT_SUCCESS)
+	{
+    	if ((ret = ftdi_write_data_submit(ftdic, dmx, size)) < 0)
+    	{
+        	fprintf(stderr, "unable to write data: %d (%s)\n", ret, ftdi_get_error_string(ftdic));
+        	ret = EXIT_FAILURE;
+    	}
+	}
+    
+    usleep(88 * 513);
+    
+	return ret;
+}
 
 int artnetReceiver(artnet_node node, void *pp, void *d) {
     artnet_packet pack = (artnet_packet) pp;
 
-    //NSLog(@"Receiving Art-Net data!");
-    //printf("Received packet sequence %d\n", pack->data.admx.sequence);
-    //printf("Received packet type %d\n", pack->type);
-    //printf("Received packet data %s\n", pack->data.admx.data);
-/*
-    dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
-
-    memcpy(data, pack->data.admx.data, 512);
-
-    dispatch_semaphore_signal(semaphore);
-  */
-
-    struct timeval t1;
-    gettimeofday(&t1, NULL);
-
-    //if (t1.tv_usec - ot.tv_usec > 30000) {
-        opendmx_send_frame(h, 512, pack->data.admx.data);
-
-        ot = t1;
-    //}
-
+    if (pack->type == 0x5000) {
+        dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
+        memcpy(data, pack->data.admx.data, 512);
+        dispatch_semaphore_signal(semaphore);
+    }
+  
     return ARTNET_EOK;
 }
 
@@ -53,6 +113,34 @@ int artnetReceiver(artnet_node node, void *pp, void *d) {
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
+    /*
+    dmx_init(&ftdic);
+
+    for (int j = 0; j < 512; j++) {
+        data[j] = 0;
+    }
+
+    for (int i = 0; i < 255; i++) {
+        for (int j = 0; j < 512; j++) {
+            
+        }
+        
+        data[0] = 0;
+        data[1] = 255;
+        data[2] = i;
+        data[3] = i;
+        data[4] = i;
+        data[5] = 0;
+        data[6] = 0;
+        
+        dmx_write(&ftdic, data, 512);
+        
+        usleep(100 * 1000);
+    }
+    
+    printf("OK");
+    exit(0);
+    //*/
     
     //dispatch_async(dispatch_get_global_queue(0, 0), ^{
         char *ip_addr = NULL;
@@ -92,42 +180,34 @@ int artnetReceiver(artnet_node node, void *pp, void *d) {
 
     semaphore = dispatch_semaphore_create(1);
 
-    h = opendmx_open();
-    gettimeofday(&ot, NULL);
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
+        unsigned char buf[513];
 
-        //dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
-            while (1) {
-                //artnet_send_poll(artnetNode, NULL, ARTNET_TTM_DEFAULT);
-                //printf("arnet_get_sd() => %i\n", artnet_get_sd(artnetNode));
-                printf("artnet_read() => %i\n", artnet_read(artnetNode, 1));
-            }
-        //});
-/*
-        //dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
+        dmx_init(&ftdic);
+        
+        while (1) {
+            
+            buf[0] = 0;
 
-            int i = 0;
-            while (1) {
-                unsigned char buf[512];
-                
-                printf("%d\n", i);
-
-                dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
-                
-                memcpy(buf, data, 512);
-                
-                dispatch_semaphore_signal(semaphore);
-                
-                opendmx_send_frame(h, 512, buf);
-                
-                i++;
-            }
-        //});
- */
+            dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
+            memcpy(buf + 1, data, 512);
+            dispatch_semaphore_signal(semaphore);
+            
+            dmx_write(&ftdic, buf, 513);
+            usleep(100 * 1000);
+        }
+    });
     
-        // Use this to deallocate memory
-        //artnet_stop(artnetNode);
-        //artnet_destroy(artnetNode);
+    
+    //dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
+        while (1) {
+            artnet_read(artnetNode, 1);
+        }
     //});
+    
+    // Use this to deallocate memory
+    //artnet_stop(artnetNode);
+    //artnet_destroy(artnetNode);
 }
 
 @end
